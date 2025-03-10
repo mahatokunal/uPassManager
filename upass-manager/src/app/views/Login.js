f001ebf80cd54c883a53fe13e23508a9f73e974b
mtirkey <mtirkey@vt.edu>
2025-03-10 17:38:42 -0400

    login page stylized

--- upass-manager/src/app/views/Login.js
+++ upass-manager/src/app/views/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 import loginImage from '../assets/images/login-page.png';
 import Image from 'next/image';
+import '../assets/colors/fonts.css'; // Import the CSS file
 
 const Login = () => {
   const router = useRouter();
@@ -20,7 +21,7 @@ const Login = () => {
           <div className="w-full max-w-md">
             <form className="space-y-4" onSubmit={handleLogin}>
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700" style={{ fontFamily: 'AcherusGrotesque-Regular', fontSize: '15px' }}>
                   Email
                 </label>
                 <input
@@ -28,11 +29,12 @@ const Login = () => {
                   id="email"
                   name="email"
                   required
-                  className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-gray-800"
+                  style={{ fontFamily: 'AcherusGrotesque-Regular' }}
                 />
               </div>
               <div>
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700" style={{ fontFamily: 'AcherusGrotesque-Regular', fontSize: '15px' }}>
                   Password
                 </label>
                 <input
@@ -40,21 +42,23 @@ const Login = () => {
                   id="password"
                   name="password"
                   required
-                  className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-gray-800"
+                  style={{ fontFamily: 'AcherusGrotesque-Regular' }}
                 />
-              </div>
-              <div className="flex items-center justify-between">
-                <a href="#" className="text-sm text-indigo-600 hover:text-indigo-500">
-                  Forgot Password?
-                </a>
               </div>
               <div>
                 <button
                   type="submit"
-                  className="w-full px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="w-full text-white bg-[#861F41] hover:bg-[#6e1a34] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#861F41]"
+                  style={{ fontFamily: 'AcherusGrotesque-Bold', width: '150px', height: '40px', fontSize: '20px', borderRadius: '15px' }}
                 >
                   Login
                 </button>
+              </div>
+              <div className="flex items-center justify-between">
+                <a href="#" className="text-sm text-indigo-600 hover:text-indigo-500" style={{ fontFamily: 'AcherusGrotesque-Regular', fontSize: '15px' }}>
+                  Forgot Password?
+                </a>
               </div>
             </form>
           </div>
