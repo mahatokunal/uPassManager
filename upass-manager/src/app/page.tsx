69f58bc8c11fae2b6511c99ed7120afb93e76aa6
mtirkey <mtirkey@vt.edu>
2025-03-10 14:47:44 -0400

    clean code

--- upass-manager/src/app/page.tsx
+++ upass-manager/src/app/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 import Header from '../app/components/Header';
 import Footer from '../app/components/Footer';
 import Login from '../app/views/Login';
 
 export default function Home() {
   const router = useRouter();
-  const [pid, setPid] = useState('');
-
-  const handleLogin = () => {
-    // Add your login logic here
-    // For now, we'll just navigate to the Dashboard
-    router.push('/dashboard');
-  };
 
   return (
     <div className="flex flex-col min-h-screen bg-white">
