f90cdb6f2012133d9403dfd207c30b3f9c1f3f49
nidhikamath2102 <kamathnidhi98@gmail.com>
2025-04-27 15:25:32 -0400

    minor change in footer

--- upass-manager/src/app/views/Distributors.js
+++ upass-manager/src/app/views/Distributors.js
@@ -217,13 +217,13 @@ const Distributors = () => {
                               </td>
                             </tr>
                           ))}
-                          <tfoot>
+                        </tbody>
+                        <tfoot>
                             <tr>
                               <td colSpan="3" className="px-6 py-4 text-center text-gray-500">
                               </td>
                             </tr>
                           </tfoot>
-                        </tbody>
                       </table>
                     </div>
                   </div>
